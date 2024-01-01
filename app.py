735e8fdcacf93cea749bdae093208d919f9d0f3e
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-01-01 20:58:50 +0000

    sa

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
 
 pinecone.init(api_key=PINECONE_API_KEY,environment=PINECONE_API_ENV)
+
 index_name = "med-chatbot"
+embeddings = download_hugging_face_embeddings()
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 
 def set_custom_prompt():    
@@ -30,14 +33,14 @@ def set_custom_prompt():
     return prompt
 
 
-def retrieval_qa_chain(llm, prompt, db):
+def retrieval_qa_chain(llm, prompt, docsearch):
     """
     Retrieval QA Chain
     """
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type='stuff',
-        retriever=db.as_retriever(search_kwargs={'k': 2}),
+        retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
         return_source_documents=True,
         chain_type_kwargs={'prompt': prompt},
         )
@@ -52,20 +55,18 @@ def load_llm():
         model = "TheBloke/Llama-2-7B-Chat-GGML",
         model_type="llama",
         max_new_tokens = 512,
-        temperature = 0.5
+        temperature = 0.5,
     )
     return llm
 
 
-def qa_bot():
+def qa_bot(index_name: str):
     """
     QA Model Function
     """
-    embeddings = download_hugging_face_embeddings()
-    db = Pinecone.from_existing_index(index_name, embeddings)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
-    qa = retrieval_qa_chain(llm, qa_prompt, db)
+    qa = retrieval_qa_chain(llm, qa_prompt, docsearch)
     return qa
 
 
@@ -73,7 +74,9 @@ def final_result(query):
     """
     Output function
     """
-    qa_result = qa_bot()
+    print("1. ")
+    qa_result = qa_bot(index_name)
+    print("2. ")
     response = qa_result({'query': query})
     return response
 
@@ -86,10 +89,14 @@ def index():
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     query = request.form["msg"]
+    print(query)
     response = final_result(query)
-    print(input)
     print("Response : ", response["result"])
     return str(response["result"])
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
